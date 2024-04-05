540822f140fbfd410f8d3d236fdd68c7e8d70c84
ErickVera123 <erick.vera@uabc.edu.mx>
2024-04-05 16:40:01 -0700

    Se agrego practica 24

---
+++ Practica 24/practica24.js
@@ -0,0 +1,11 @@
+//creacion de la funcion
+function avisohuracan(hayUnHuracan) {
+    if (hayUnHuracan) {
+        alert ("Hay un huracan");
+    } else {
+        alert ("Esta soleado");
+    }
+}
+
+var hayUnHuracan = false;
+alert(avisohuracan(hayUnHuracan));
